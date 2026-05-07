1f5cb1cfd5cac8f0a614abaf2451c314e50e462c
GMSKrishna <manikantasaikrishna8125@gmail.com>
2026-05-07 17:55:27 +0530

    Configured project for Render deployment

--- Desktop/real-time-data-pipeline/database/db.py
+++ Desktop/real-time-data-pipeline/database/db.py
@@ -1,5 +1,6 @@
+import os
 from sqlalchemy import create_engine
 
-DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/realtime_pipeline"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
